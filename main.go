8b00a9f4cc135db2f5e3927b8dd4521e671cd242
Aleks Saul <asaul@pivotal.io>
2019-08-19 14:06:22 -0400

    debug info

--- main.go
+++ main.go
@@ -18,12 +18,13 @@ func handle(w http.ResponseWriter, r *http.Request) {
 	appEnv, _ := cfenv.Current()
 	fmt.Println("Services:", appEnv.Services)
 	fmt.Fprintf(w, "vcap_services is %s\n", os.Getenv("VCAP_SERVICES"))
-	vcConf := vault.NewConfig()
+	//vcConf := vault.NewConfig()
+	_ := vault.NewConfig()
 	cfvault, err := appEnv.Services.WithName("hashicorp-vault")
 	if err != nil {
 		fmt.Println("Error getting Vault Service from VCAP : %s", err)
 	}
-
-	vcConf.Address = cfvault.Credentials
+	fmt.Println("cfvault.Credentials : %s", cfvault.Credentials)
+	//vcConf.Address = cfvault.Credentials
 
 }
